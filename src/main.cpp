b82dec5efaa993c327f400ae2d22ea8352f6e7e9
Jun Chen <jun@JundeMac.local>
2014-01-11 14:33:38 +0800

    Support C++11

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <vector>
+using namespace std;
 
-TEST(A, B) { SUCCEED(); }
+TEST(A, B) {
+    vector<int> a{1,2,3,4,5};
+    ASSERT_EQ(5, a.size());
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
