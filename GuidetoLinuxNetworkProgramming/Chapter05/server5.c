0e40de9a9346c508b12d6245612cd8638fc55409
Jun Chen <jun@JundeMac.local>
2014-03-22 22:25:10 +0800

    Add the source code of Effective TCP/IP Programming

--- GuidetoLinuxNetworkProgramming/Chapter05/server5.c
+++ GuidetoLinuxNetworkProgramming/Chapter05/server5.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <unistd.h>
 
 void* thread_proc(void *arg);
 
@@ -18,62 +19,64 @@ int main(int argc, char *argv[])
     int val;
     
     if (argc > 1) {
-      nchildren = atoi(argv[1]);
+        nchildren = atoi(argv[1]);
     }
-
+    
     listensock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+    
     val = 1;
     result = setsockopt(listensock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
     if (result < 0) {
         perror("server5");
         return 0;
     }
-
+    
     sAddr.sin_family = AF_INET;
     sAddr.sin_port = htons(1972);
     sAddr.sin_addr.s_addr = INADDR_ANY;
-
+    
     result = bind(listensock, (struct sockaddr *) &sAddr, sizeof(sAddr));
     if (result < 0) {
         perror("exserver5");
         return 0;
     }
-
+    
     result = listen(listensock, 5);
     if (result < 0) {
         perror("exserver5");
         return 0;
     }
-
-   for (x = 0; x < nchildren; x++) {
-	result = pthread_create(&thread_id, NULL, thread_proc, (void *) listensock);
-	if (result != 0) {
-	  printf("Could not create thread.\n");
-	  return 0;
-	}
-	sched_yield();
+    
+    for (x = 0; x < nchildren; x++) {
+        result = pthread_create(&thread_id, NULL, thread_proc, (void *) listensock);
+        if (result != 0) {
+            printf("Could not create thread.\n");
+            return 0;
+        }
+        sched_yield();
     }
-
-   pthread_join (thread_id, NULL);
+    
+    pthread_join (thread_id, NULL);
 }
 
 void* thread_proc(void *arg)
 {
-  int listensock, sock;
-  char buffer[25];
-  int nread;
+    int listensock, sock;
+    char buffer[25];
+    int nread;
+    
+    listensock = (int) arg;
+    
+    while (1) {
+        sock = accept(listensock, NULL, NULL);
+        printf("client connected to child thread %i with pid %i.\n", pthread_self(), getpid());
+        nread = recv(sock, buffer, 25, 0);
+        buffer[nread] = '\0';
+        printf("%s\n", buffer);
+        send(sock, buffer, nread, 0);
+        close(sock);
+        printf("client disconnected from child thread %i with pid %i.\n", pthread_self(), getpid());
+    }
 
-  listensock = (int) arg;
-
-  while (1) {
-    sock = accept(listensock, NULL, NULL);
-    printf("client connected to child thread %i with pid %i.\n", pthread_self(), getpid());
-    nread = recv(sock, buffer, 25, 0);
-    buffer[nread] = '\0';
-    printf("%s\n", buffer);
-    send(sock, buffer, nread, 0);
-    close(sock);
-    printf("client disconnected from child thread %i with pid %i.\n", pthread_self(), getpid());
-  }
+    return NULL;
 }
